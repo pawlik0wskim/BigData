84a6ee1d756a530d48d3f95d8e5712ab4cf6734e
Kacper Kurowski <43794510+Korigami@users.noreply.github.com>
2023-01-09 21:22:32 +0100

    Add files via upload

--- cpik/generate_data.py
+++ cpik/generate_data.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import csv
+import re
 from unidecode import unidecode
 from datetime import datetime, timedelta
 
 def filter_line(line):
     return [line[i] for i in range(len(line)) if i not in out_idx]
+
+def get_col_idx(col_name):
+    idx = None
+    for i in range(len(header)):
+        if header[i] == col_name:
+            idx = i
+            break
+    return idx
 
 dates = pd.date_range(datetime.strptime('2019-01-01', '%Y-%m-%d'),
               datetime.strptime('2020-01-01', '%Y-%m-%d')-timedelta(days=1),freq='d')
@@ -12,12 +21,10 @@ f = open('/home/vagrant/cpik/pojazdy_14_2022-04-17.csv', 'r', encoding='utf-8')
 folder = '/home/vagrant/generated_data/'
 header = list(csv.reader([f.readline()]))[0]
 
-idx = 0
-# find index of registration date
-for i in range(len(header)):
-    if header[i] == 'data_rejestracji_ost':
-        idx = i
-        break
+reg_date_idx = get_col_idx('data_rejestracji_ost')
+prod_year_idx = get_col_idx('rok_produkcji') 
+prod_type_idx = get_col_idx('sposob_produkcji')
+
 out_cols = ['typ', 'model', 'wariant', 'wersja']
 out_idx = [i for i in range(len(header)) if header[i] in out_cols]
  
@@ -35,10 +42,14 @@ while True:
     line = f.readline()
     if line == '':
         break
-    line = list(csv.reader([line]))[0]    
-    if '2019-01-01' <= line[idx] <= '2019-12-31':
+    line = list(csv.reader([line]))[0]
+
+    if not re.match('^\d+$', line[prod_year_idx]):
+        line[prod_year_idx], line[prod_type_idx] = line[prod_type_idx], line[prod_year_idx]        
+     
+    if '2019-01-01' <= line[reg_date_idx] <= '2019-12-31':
         line = [unidecode(line[i]) for i in range(len(line))] 
-        csv.writer(partitions[line[idx]], lineterminator='\n').writerow(filter_line(line))
+        csv.writer(partitions[line[reg_date_idx]], lineterminator='\n').writerow(filter_line(line))
 
 # close everything
 f.close()
